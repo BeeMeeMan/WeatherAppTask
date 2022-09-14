0e97c43c2bc02aa11c3a73458de2a79c885f190f
Korsun <jenya.korsun@gmail.com>
2022-09-14 18:25:53 +0300

    Set IOS version. Remove storyboard. Add project folders.

--- WeatherAppTask/Presentation/MainWeatherViewController.swift
+++ WeatherAppTask/Presentation/MainWeatherViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainWeatherViewController.swift
 //  WeatherAppTask
 //
 //  Created by Yevhenii Korsun on 14.09.2022.
@@ -7,13 +7,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainWeatherViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
